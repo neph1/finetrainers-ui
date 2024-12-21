5c5a6722e63af45ec45e823a0a30d6d3ef13a01c
rickard <rickard@mindemia.com>
2024-12-21 15:13:51 +0100

    general run tab

--- tabs/training_tab_legacy.py
+++ tabs/training_tab_legacy.py
@@ -4,14 +4,13 @@ import gradio as gr
 from typing import OrderedDict
 from config import Config, global_config
 
-from run_trainer import RunTrainer
 from runner import RunCogVideoX
 from tabs import general_tab
 from tabs.tab import Tab
 
 properties = OrderedDict()
 
-class TrainingTab(Tab):
+class LegacyTrainingTab(Tab):
 
     def __init__(self, title, config_file_path, allow_load=False):
         super().__init__(title, config_file_path, allow_load)
@@ -30,8 +29,8 @@ class TrainingTab(Tab):
             self.add_buttons()
 
         self.output_box = gr.Textbox(value="", label="Output")
-        run_button = gr.Button("Start Training", key='run_trainer')
-        run_button.click(self.run_trainer, 
+        run_button = gr.Button("Run Training", key='run_cogvideox')
+        run_button.click(self.run_cogvideox, 
                         inputs=[*properties.values()],
                         outputs=[self.output_box]
                         )
@@ -39,7 +38,7 @@ class TrainingTab(Tab):
     def get_properties(self) -> OrderedDict:
         return properties
     
-    def run_trainer(self, *args):
+    def run_cogvideox(self, *args):
         properties_values = list(args)
         keys_list = list(properties.keys())
         
@@ -50,10 +49,9 @@ class TrainingTab(Tab):
             config.set(key, properties_values[index])
         output_path = os.path.join(properties['output_dir'].value, "config")
         os.makedirs(output_path, exist_ok=True)
-        result = RunTrainer().run(config, general_tab.properties['path_to_cogvideox_factory'].value)
-        if isinstance(result, str):
-            return result
+        result = RunCogVideoX().run_cogvideox(config, general_tab.properties['path_to_cogvideox_factory'].value)
         if result.returncode == 0:
             return "Run Training: Training completed successfully"
-        return "Run Training: Training failed"
+        else:
+            return "Run Training: Training failed"
     
